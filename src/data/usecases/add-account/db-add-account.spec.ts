8eaa73d76cb770295e1c373a82590f8f2b572839
Luan Rafael <luanrafaelberto@gmail.com>
2023-03-15 09:26:10 -0400

    refactor: move sut creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,15 +1,28 @@
 import { type Hasher } from '../../protocols/hasher'
 import { DbAddAccount } from './db-add-account'
 
+interface SutTypes {
+  sut: DbAddAccount
+  hasherStub: Hasher
+}
+
+const makeSut = (): SutTypes => {
+  class HasherStub implements Hasher {
+    async hash (value: string): Promise<string> {
+      return await new Promise(resolve => { resolve('hashed_password') })
+    }
+  }
+  const hasherStub = new HasherStub()
+  const sut = new DbAddAccount(hasherStub)
+  return {
+    sut,
+    hasherStub
+  }
+}
+
 describe('DbAddAccount Usecase', () => {
   test('Should call Hasher with correct password', async () => {
-    class HasherStub implements Hasher {
-      async hash (value: string): Promise<string> {
-        return await new Promise(resolve => { resolve('hashed_password') })
-      }
-    }
-    const hasherStub = new HasherStub()
-    const sut = new DbAddAccount(hasherStub)
+    const { sut, hasherStub } = makeSut()
     const hasherSpy = jest.spyOn(hasherStub, 'hash')
     const accountData = {
       name: 'valid_name',

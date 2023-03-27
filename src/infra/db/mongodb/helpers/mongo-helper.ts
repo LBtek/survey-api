507b9bb62cdc6086eedc58e4b41c982449d9cbd3
Luan Rafael <luanrafaelberto@gmail.com>
2023-03-27 10:28:25 -0400

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { type Collection, MongoClient, type InsertOneResult } from 'mongodb'
+import { type Collection, MongoClient, type InsertOneResult, type WithId, type Document } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -21,7 +21,12 @@ export const MongoHelper = {
     return this.client.db().collection(name)
   },
 
-  mapOneResult (result: InsertOneResult, restData: object): any {
+  mapInsertOneResult (result: InsertOneResult<Document>, restData: object): any {
     return { id: result.insertedId.toString(), ...restData }
+  },
+
+  mapOneDocumentWithId (result: WithId<Document>): any {
+    const { _id, ...rest } = result
+    return { id: _id.toString(), ...rest }
   }
 }

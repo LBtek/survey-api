1f950f5ff7d89f1253181b43b69994b289807f0f
Luan Rafael <luanrafaelberto@gmail.com>
2023-04-20 18:26:49 -0400

    refactor: organize main layer folders

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -1,6 +1,6 @@
 import { type Controller } from '@/presentation/protocols'
 import { AddSurveyController } from '@/presentation/controllers/survey/add-survey/add-survey-controller'
-import { makeDbAddSurvey } from '@/main/factories/usecases/add-survey/db-add-survey-factory'
+import { makeDbAddSurvey } from '@/main/factories/usecases/survey/add-survey/db-add-survey-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
 

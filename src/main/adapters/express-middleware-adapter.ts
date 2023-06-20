24c7c13aa609dc1f9a2c92ab05238ec63b4b5fb5
Luan Rafael <luanrafaelberto@gmail.com>
2023-06-20 17:18:13 -0400

    refactor: change argument name in adaptMiddleware

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -1,13 +1,13 @@
 import { type Response, type Request, type NextFunction } from 'express'
 import { type HttpRequest, type Middleware } from '@/presentation/protocols'
 
-export const adaptMiddleware = (controller: Middleware) => {
+export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const httpRequest: HttpRequest = {
       headers: req.headers,
       body: req.body
     }
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       Object.assign(req, httpResponse.body)
       next()

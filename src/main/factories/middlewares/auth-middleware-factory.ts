fbd433d6f02167a74918a249a252be0baa221859
Luan Rafael <luanrafaelberto@gmail.com>
2023-09-25 18:43:38 -0400

    refactor: update auth middleware factory

--- src/main/factories/middlewares/auth-middleware-factory.ts
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -2,9 +2,10 @@ import { type Account } from '@/application/entities'
 import { type Middleware } from '@/presentation/protocols'
 import { AuthMiddleware } from '@/presentation/middlewares/auth-middleware'
 import { makeLogAuthMiddlewareDecorator } from '../decorators'
-import { makeDbLoadUserByAccountAccessToken } from '../services/app-services'
+import { makeExtractAccessTokenPayloadService, makeLoadAuthenticatedUserByTokenService } from '../services/app-services'
+import { makeCheckAndRefreshTokenService } from '../services/app-services/check-and-refresh-access-token-factory'
 
 export const makeAuthMiddleware = (roles?: Set<Account.BaseDataModel.Roles>): Middleware => {
-  const middleware = new AuthMiddleware(makeDbLoadUserByAccountAccessToken(), roles)
+  const middleware = new AuthMiddleware(makeExtractAccessTokenPayloadService(), makeLoadAuthenticatedUserByTokenService(), makeCheckAndRefreshTokenService(), roles)
   return makeLogAuthMiddlewareDecorator(middleware)
 }

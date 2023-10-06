41eddd3ed08ff3b0607206ef9d8fa570fa2dfdf3
Luan Rafael <luanrafaelberto@gmail.com>
2023-10-06 15:13:30 -0400

    feat: add swagger api docs

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,6 @@
-import ip from 'ip'
-
 export default {
   port: process.env.PORT || 5050,
-  apiHost: process.env.API_HOST || ip?.address() || '127.0.0.1',
+  apiHost: process.env.API_HOST || '127.0.0.1',
   nodeEnv: process.env.NODE_ENV || 'development',
   mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api',
   jwtSecret: process.env.JWT_SECRET || '--!@bC3$c$2B1a#-=='

2d0538763f5bd09cafd9145582d128cd9169b586
Luan Rafael <luanrafaelberto@gmail.com>
2023-11-19 15:23:14 -0400

    chore: add traefik as reverse proxy and add redis

---
+++ src/infra/db/in-memory/redis/RedisClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from 'redis'
+import env from '@/main/config/env'
+
+export const RedisClient = createClient({
+  url: env.redis.url
+})
+  .on('error', err => { console.log('Redis Client Error', err) })

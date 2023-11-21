3775a545f5a7531d23e10c62b25a7c219f987393
Luan Rafael <luanrafaelberto@gmail.com>
2023-11-21 15:48:00 -0400

    refactor: change configs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:19.7
 RUN npm install -g npm@^9
-WORKDIR /usr/src/clean-node-api
+WORKDIR /usr/src/survey-api
 COPY ./package.json .
 COPY ./package-lock.json .
 RUN npm pkg delete scripts.prepare
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,13 +2,13 @@ export default {
   nodeEnv: process.env.NODE_ENV || 'development',
   api: {
     port: process.env.PORT || 5050,
-    host: process.env.API_HOST || '127.0.0.1',
+    host: process.env.API_HOST || 'localhost',
     jwtSecret: process.env.JWT_SECRET || '--!@bC3$c$2B1a#-=='
   },
   mongodb: {
-    url: process.env.MONGO_URL || 'mongodb://mongo:27017/survey-api'
+    url: process.env.MONGO_URL || 'mongodb://localhost:27017/survey-api'
   },
   redis: {
-    url: process.env.REDIS_URL || 'redis://default:AbcD1234@redis:6379'
+    url: process.env.REDIS_URL || 'redis://default:AbcD1234@localhost:6379'
   }
 }

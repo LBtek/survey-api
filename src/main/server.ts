bf4c4cb0a23f0e2e616257e55b903d0b42feabed
Luan Rafael <luanrafaelberto@gmail.com>
2023-11-21 15:03:07 -0400

    refactor: change server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,17 +4,14 @@ import { RedisClient } from '@/infra/db/in-memory/redis/RedisClient'
 import { checkServerIsOk } from '@/infra/http/requests/check-server-is-ok'
 import env from '@/main/config/env'
 
-MongoHelper.connect(env.mongodb.url)
-  .then(async () => {
-    const app = (await import('./config/app')).default
+Promise.all([
+  MongoHelper.connect(env.mongodb.url),
+  RedisClient.connect()
+]).then(async () => {
+  const app = (await import('./config/app')).default
 
-    RedisClient.connect()
-      .then(() => {
-        app.listen(env.api.port, () => {
-          console.log(`Server running at http://${env.api.host}:${env.api.port}`)
-          void checkServerIsOk.sendRequest()
-        })
-      })
-      .catch(console.error)
+  app.listen(env.api.port, () => {
+    console.log(`Server running at http://${env.api.host}:${env.api.port}`)
+    void checkServerIsOk.sendRequest()
   })
-  .catch(console.error)
+}).catch(console.error)

73b2830574e7fdeee7aa800586cea21b40a54378
Luan Rafael <luanrafaelberto@gmail.com>
2023-12-31 12:22:22 -0400

    feat: add PublisherLoadSurveys route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -4,12 +4,14 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import {
   makePublisherAddSurveyController,
   makeUserLoadOneSurveyController,
-  makeUserLoadAllSurveysController
+  makeUserLoadAllSurveysController,
+  makePublisherLoadSurveysController
 } from '../factories/controllers'
 import { authUser, authPublisher } from '../middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/publisher/surveys', authPublisher, adaptRoute(makePublisherAddSurveyController()))
+  router.get('/publisher/surveys', authPublisher, adaptRoute(makePublisherLoadSurveysController()))
   router.get('/user/surveys/:surveyId', authUser, adaptRoute(makeUserLoadOneSurveyController()))
   router.get('/user/surveys', authUser, adaptRoute(makeUserLoadAllSurveysController()))
 }

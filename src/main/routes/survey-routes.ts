a0818f4344b2c4a0fb5f0fc60a2771962c9e69e2
Luan Rafael <luanrafaelberto@gmail.com>
2024-01-02 15:58:44 -0400

    feat: add GuestLoadOneSurvey route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -5,13 +5,15 @@ import {
   makePublisherAddSurveyController,
   makeUserLoadOneSurveyController,
   makeUserLoadAllSurveysController,
-  makePublisherLoadSurveysController
+  makePublisherLoadSurveysController,
+  makeGuestLoadOneSurveyController
 } from '../factories/controllers'
 import { authUser, authPublisher } from '../middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/publisher/surveys', authPublisher, adaptRoute(makePublisherAddSurveyController()))
   router.get('/publisher/surveys', authPublisher, adaptRoute(makePublisherLoadSurveysController()))
+  router.get('/guest/surveys/:surveyId', adaptRoute(makeGuestLoadOneSurveyController()))
   router.get('/user/surveys/:surveyId', authUser, adaptRoute(makeUserLoadOneSurveyController()))
   router.get('/user/surveys', authUser, adaptRoute(makeUserLoadAllSurveysController()))
 }

12ab196f331f36abf785d685321c591ba6113ac9
Luan Rafael <luanrafaelberto@gmail.com>
2024-01-09 18:05:30 -0400

    refactor: improve error messages returned from zod validation

--- src/application/validation/validators/zod-validation.ts
+++ src/application/validation/validators/zod-validation.ts
@@ -10,7 +10,9 @@ export class ZodValidation implements IValidation {
   validate (input: object): Error {
     const result = this.zodValidatorSchema.safeParse(input)
     if (result.success === false) {
-      return result.error
+      const error = new Error()
+      error.message = result.error.errors[0].message
+      return error
     }
     return null
   }

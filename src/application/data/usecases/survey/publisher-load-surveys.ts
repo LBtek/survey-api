d4f85aa54c13f1c61fd72927cae0fd8d969589a9
Luan Rafael <luanrafaelberto@gmail.com>
2024-01-11 16:08:26 -0400

    feat: returns percentage on PublisherLoadSurveys

--- src/application/data/usecases/survey/publisher-load-surveys.ts
+++ src/application/data/usecases/survey/publisher-load-surveys.ts
@@ -1,12 +1,16 @@
 import { type PublisherLoadSurveys as PublisherLoadSurveysModel } from '@/domain/models'
 import { type IPublisherLoadSurveys } from '@/domain/usecases/publisher-context'
 import { type IPublisherLoadSurveysRepository } from '../../protocols/repositories'
+import { addPercentageToAnswers } from '../../helpers'
 
 export class PublisherLoadSurveys implements IPublisherLoadSurveys {
   constructor (private readonly publisherLoadSurveysRepository: IPublisherLoadSurveysRepository) { }
 
   async load (data: PublisherLoadSurveysModel.Params): Promise<PublisherLoadSurveysModel.Result> {
     const surveys = await this.publisherLoadSurveysRepository.publisherLoadSurveys(data)
-    return surveys
+
+    const surveysWithPercent = surveys.map(survey => addPercentageToAnswers(survey)) as any
+
+    return surveysWithPercent
   }
 }

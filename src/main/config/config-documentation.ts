ce8bfe82a2a8d9c82423e8cf6f9196fbaf1b227a
Luan Rafael <luanrafaelberto@gmail.com>
2024-02-08 09:40:09 -0400

    chore: add dynamic redoc api documentation

--- src/main/config/config-documentation.ts
+++ src/main/config/config-documentation.ts
@@ -1,13 +1,27 @@
-import express, { type Express } from 'express'
+import { type Express } from 'express'
 import { serve, setup } from 'swagger-ui-express'
 import { noCache } from '@/main/middlewares/no-cache'
-import documentationConfig from '@/main/docs'
 import { resolve } from 'path'
+import Handlebars from 'handlebars'
+import documentationConfig from '@/main/docs'
+import fs from 'fs'
+
+Handlebars.registerHelper('link', function (text: string, url: string) {
+  url = Handlebars.escapeExpression(url)
+  text = Handlebars.escapeExpression(text)
+  return new Handlebars.SafeString("<a href='" + url + "'>" + text + '</a>')
+})
 
 export default (app: Express): void => {
   app.use('/api-swagger-doc', noCache, serve, setup(documentationConfig))
   app.get('/api-documentation', noCache, (request, response) => {
     return response.send(JSON.stringify(documentationConfig))
   })
-  app.use('/api-redoc', noCache, express.static(resolve(__dirname, '../docs/redoc')))
+  app.use('/api-redoc', noCache, (req, response) => {
+    const template = fs.readFileSync(`${resolve(__dirname, '../docs/redoc')}/index.hbs`, 'utf8')
+    const compiled = Handlebars.compile(template)
+    const url = `${req.protocol + '://' + req.get('host')}/api-documentation`
+    const html = compiled({ url })
+    return response.send(html)
+  })
 }

b706071ce06ccf41237ff520d753641c1ba8927c
Luan Rafael <luanrafaelberto@gmail.com>
2024-06-21 17:07:47 -0400

    fix: adapt code front end

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,6 +2,21 @@
 import { type Express, Router } from 'express'
 import { readdirSync } from 'fs'
 
+const month = {
+  '1': 'janeiro',
+  '2': 'fevereiro',
+  '3': 'março',
+  '4': 'abril',
+  '5': 'maio',
+  '6': 'junho',
+  '7': 'julho',
+  '8': 'agosto',
+  '9': 'setembro',
+  '10': 'outubro',
+  '11': 'novembro',
+  '12': 'dezembro',
+}
+
 const generateDuties = require('../../../duty-roster/index.js')
 
 export default (app: Express): void => {
@@ -9,6 +24,7 @@ export default (app: Express): void => {
   app.use('/api', router)
     /* istanbul ignore next */
   router.post('/duty', (req, res) => {
+    console.log(req.body)
     if (
       req.body &&
       req.body.dates &&
@@ -17,7 +33,38 @@ export default (app: Express): void => {
       req.body.vacationsEmp &&
       req.body.vacationsPO
     ) {
-      const { dates, employees, bailiffs, vacationsEmp, vacationsPO } = req.body
+      const { dates, employees, bailiffs } = req.body
+
+      const vacationsEmp = {}
+      const vacationsPO = {}
+
+      req.body.vacationsEmp.forEach((emp) => {
+        vacationsEmp[emp.name] = []
+        emp.absences?.forEach((abs) => {
+          const vacation = {}
+          const start = new Date(abs.start_date)
+          const end = new Date(abs.end_date)
+          vacation['startDay'] = start.getDate()
+          vacation['startMonth'] = month[(start.getMonth()+1).toString()]
+          vacation['endDay'] = end.getDate()
+          vacation['endMonth'] = month[(end.getMonth()+1).toString()]
+          vacationsEmp[emp.name].push(vacation)
+        })
+      })
+
+      req.body.vacationsPO.forEach((po) => {
+        vacationsPO[po.name] = []
+        po.absences?.forEach((abs) => {
+          const vacation = {}
+          const start = new Date(abs.start_date)
+          const end = new Date(abs.end_date)
+          vacation['startDay'] = start.getDate()
+          vacation['startMonth'] = month[(start.getMonth()+1).toString()]
+          vacation['endDay'] = end.getDate()
+          vacation['endMonth'] = month[(end.getMonth()+1).toString()]
+          vacationsPO[po.name].push(vacation)
+        })
+      })
 
       const lastDayWorkedEmp = {}
       const lastDayWorkedPO = {}
